8f835af5119416101b923d38bca719c85f5d2325
mitch <mgregoris2@myseneca.ca>
2024-09-23 11:02:22 -0400

    Lab-02-E completed

---
+++ lab2e.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+# Author: Mitch Gregoris
+# Author ID: 133349191
+# Date Created: 2023-09-23
+
+import sys
+
+timer = int(10)
+
+while(timer != 0):
+    print(timer)
+    timer = timer - 1
+print("blast off!")    
