a97237137c529a9f6cdec052d21c96c93c20a571
mitch <mgregoris2@myseneca.ca>
2024-09-25 23:22:53 -0400

    clean up

--- lab2b.py
+++ lab2b.py
@@ -1,12 +1,7 @@
 #!/usr/bin/env python3
 
 name = input("Name: ")
-#name = input()
 
 age = input("Age: ")
-
-#age = input()
-
-#print('Hi ' + name + ', you are ' + str(age) + ' years old.')
 
 print('Hi ' + name + ', you are ' + str(age) + ' years old.')
